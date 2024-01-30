8ad1aeddba57338ff48fc1a3dbb3e0c0f1c11085
Muhammad Abdullah <dev.mirxa009@gmail.com>
2024-01-30 21:19:13 +0500

    example

---
+++ bigOn.js
@@ -0,0 +1,15 @@
+const names=['memo'];
+
+const moreNames= new Array(1000).fill('memo')
+
+function memofinder(array){
+for(let i=0; i<array.length;i++){
+
+    if(array[i]==='memo'){
+        console.log("memo find at index: "+ i )
+    }
+}
+let te = performance.now();
+
+}
+memofinder(moreNames);
